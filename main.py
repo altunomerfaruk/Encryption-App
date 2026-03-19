32b754f36c3e85871985704b4391a41470c35d31
Ömer Faruk Altun <altunomerfarukk@gmail.com>
2026-03-20 00:00:01 +0300

    First Version

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter
+screen = tkinter.Tk()
+screen.title("Encryption App")
+screen.minsize(400, 300)
+
+title = tkinter.Label(screen, text="Encryption App", font=("Arial", 25))
+title.pack()
+
+
+
+
+
+
+
+screen.mainloop()
